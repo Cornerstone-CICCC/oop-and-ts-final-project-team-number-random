1b8cc28e7e62c014fb99fde8ba5d0fdab931c1f8
Hiroki Inoue <tyynh.hiroki@gmail.com>
2026-02-25 20:52:35 -0800

    Chore / Added a board title

--- src/features/kanban/components/KanbanBoard.tsx
+++ src/features/kanban/components/KanbanBoard.tsx
@@ -3,7 +3,9 @@ import { SearchBar } from "@/components";
 export function KanbanBoard() {
   return (
     <div className="flex h-[60vh] md:h-[calc(100vh-8rem)]">
-      <div className="bg-white rounded-xl shadow-sm border border-slate-200 w-full text-slate-400">
+
+      <div className="bg-white rounded-xl shadow-sm border border-slate-200 w-full text-black">
+        <h2 className="w-full flex items-center p-4 sm:text-4xl text-2xl">CICCC Board</h2>
         <SearchBar />
       </div>
     </div>
